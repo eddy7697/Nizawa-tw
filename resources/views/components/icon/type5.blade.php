08ed9297a81b66b8f22c0ca6e9bca9bd7fe351d0
Eddy <vincent7697@gmail.com>
2019-04-10 14:54:38 +0800

    update

---
+++ resources/views/components/icon/type5.blade.php
@@ -0,0 +1,11 @@
+<svg class="svg-icon" id="cate-03-icon_1" data-name="cate-03-icon 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
+    <defs>
+        <style>.cls-1{fill:#231815;}.cls-2{font-size:37.41px;fill:#0BA29B;stroke-width:0.25px;font-family:MicrosoftJhengHeiRegular, Microsoft JhengHei;}.cls-2,.cls-3{stroke:#fff;stroke-miterlimit:10;}.cls-3{fill: #0BA29B;stroke-width:4px;}</style>
+    </defs>
+    <title>icon-03</title>
+    <path class="cls-3" d="M155,57l44.91,22.45a11.23,11.23,0,0,1,6.23,10V143a11.23,11.23,0,0,1-6.23,10L155,175.48a11.23,11.23,0,0,1-10,0L100,153a11.23,11.23,0,0,1-6.17-10.1V89.48a11.23,11.23,0,0,1,6.23-10L145,57A11.23,11.23,0,0,1,155,57Z"/>
+    <polyline class="cls-3" points="95.66 83.41 150 110.58 204.34 83.41"/>
+    <line class="cls-3" x1="150" y1="176.6" x2="150" y2="110.58"/>
+    <line class="cls-3" x1="121.93" y1="68.48" x2="178.07" y2="96.55"/>
+    <text class="cls-2" transform="translate(75.18 238.89)" font-weight="bold">製程品管</text>
+</svg>
