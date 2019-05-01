081bc7b1395bf42eb370e24c41fe258906dbdbce
Eddy - 小茗 <vincent7697@gmail.com>
2019-05-02 04:31:27 +0800

    page methiods

--- resources/views/frontend/industry/industrialEngineering.blade.php
+++ resources/views/frontend/industry/industrialEngineering.blade.php
@@ -18,4 +18,12 @@
         </div>
     </div>
 </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 about-content">
+            <h3 style="text-align: center;margin-top: 50px;">內容建置中</h3>
+            <img src="/img/site-logo/findmore.png" style="max-width: 768px;margin-top: 0" class="about-logo" alt="">
+        </div>
+    </div>
+</div>
 @endsection
